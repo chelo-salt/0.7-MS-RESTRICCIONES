3f6a02bdcbb38c5f5100d4b694831fc85614c463
TomP22 <to.penap@duocuc.cl>
2026-06-24 23:18:33 -0400

    chore: separar configuraciones en perfil de desarrollo y conexion a Eureka

--- src/main/java/cl/municipalidad/restricciones/RestriccionesApplication.java
+++ src/main/java/cl/municipalidad/restricciones/RestriccionesApplication.java
@@ -2,8 +2,11 @@ package cl.municipalidad.restricciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class RestriccionesApplication {
 
 	public static void main(String[] args) {
